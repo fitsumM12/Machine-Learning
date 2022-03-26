e7f1150b64ae78678f3dc285c35325fd3932050f
fitsumM12 <fitsummesfindejene@gmail.com>
2022-03-26 21:57:19 +0530

    update

---
+++ PYTHON BASICS/SET/SETS.py
@@ -0,0 +1,21 @@
+"""
+This file contain basics of SET:
+    -Sets are used to store multiple items in a single variable.
+    -Set is one of 4 built-in data types in Python used to store collections of data.
+    -A set is a collection which is unordered, unchangeable*, and unindexed.
+    -Sets are written with curly brackets.
+NB: Set items are unchangeable, but you can remove items and add new items.
+
+"""
+set01 = {"fitsum","misrak","solomon","dawit"}
+print(set01)
+
+"""Note: Sets are unordered, so you cannot be sure in which order the items will appear."""
+"""Set items are unordered, unchangeable, and do not allow duplicate values.
+Unordered:
+    -Unordered means that the items in a set do not have a defined order.
+    -Set items can appear in a different order every time you use them, and cannot be referred to by index or key.
+Unchangeable:
+    -Set items are unchangeable, meaning that we cannot change the items after the set has been created.
+    -Once a set is created, you cannot change its items, but you can remove items and add new items.
+"""
